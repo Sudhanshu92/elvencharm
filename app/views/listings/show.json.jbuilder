1393c1f4448a33ee1d424b169e0cfe56ccb07852
Prelang Builder <builder@prelang.com>
2014-06-23 13:27:50 +0000

    Generated scaffolding for model 'Listing' via Rails

---
+++ app/views/listings/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @listing, :id, :title, :description, :link, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Elvencharm::Application.routes.draw do
+  resources :listings
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
