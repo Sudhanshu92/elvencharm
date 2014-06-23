cca595ee07104bd57eaadb9bfdd46c6a0afc387c
Prelang Builder <builder@prelang.com>
2014-06-23 13:27:51 +0000

    Set up member route for #vote action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Elvencharm::Application.routes.draw do
-  resources :listings
+  resources :listings do
+  #->Prelang (voting/acts_as_votable)
+  member do
+    get "vote"
+  end
+end
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
